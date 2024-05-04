017ee84fb82c4b46573cfef49c86c8879762428d
Izzat <124386339+Izzat-Engida@users.noreply.github.com>
2024-05-04 11:36:52 +0300

    Delete AP project directory

--- AP project/AuctionServer.java
+++
@@ -1,7 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface AuctionServer extends Remote {
-    boolean register(Register user) throws RemoteException;
-    boolean login(String email,String password) throws  RemoteException;
-}
