87cc59cfe50110bb42a626b72283b8e1335cb89a
Izzat <124386339+Izzat-Engida@users.noreply.github.com>
2024-05-04 13:47:57 +0300

    Update AuctionServer.java

--- AP project/AuctionServer.java
+++ AP project/AuctionServer.java
@@ -4,4 +4,8 @@ import java.rmi.RemoteException;
 public interface AuctionServer extends Remote {
     boolean register(Register user) throws RemoteException;
     boolean login(String email,String password) throws  RemoteException;
+    void removeAccount(String email,String password) throws  RemoteException;
+    ArrayList<Product> getAllProducts() throws RemoteException;
+    Product getProductDetails(int productId) throws RemoteException;
+    
 }
